852a9262068d590de3c5bcda332789f450fc3626
kenjima3301 <danhtran3301@gmail.com>
2024-11-07 20:58:32 +0700

    .

--- app/src/main/java/com/example/mobileapp/CalendarFragment.java
+++ app/src/main/java/com/example/mobileapp/CalendarFragment.java
@@ -1,16 +1,13 @@
 package com.example.mobileapp;
 
-import android.media.metrics.Event;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import androidx.fragment.app.Fragment;
 
 import java.util.ArrayList;
 import java.util.List;
